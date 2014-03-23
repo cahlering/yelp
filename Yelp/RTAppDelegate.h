2cc352bea26ea10f08edd3ac5005cccccef7fb79
cahlering <cahlering@richrelevance.com>
2014-03-23 08:48:55 -0700

    Initial Commit

---
+++ Yelp/RTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RTAppDelegate.h
+//  Yelp
+//
+//  Created by Chris Ahlering on 3/23/14.
+//  Copyright (c) 2014 Chris Ahlering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yelp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yelp
+//
+//  Created by Chris Ahlering on 3/23/14.
+//  Copyright (c) 2014 Chris Ahlering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTAppDelegate class]));
+    }
+}
